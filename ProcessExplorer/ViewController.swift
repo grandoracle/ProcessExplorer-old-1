43c64bc1a5c7793478d1099bb581509cabe87595
Kevin Walker <sage@astutestudios.com>
2021-03-13 22:36:41 -0800

    Initial Commit

---
+++ ProcessExplorer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ProcessExplorer
+//
+//  Created by Kevin Walker on 3/13/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
